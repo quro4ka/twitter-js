b059aeaff92394fafeb8665bf131459f72a31562
quro4ka <quro4ka@gmail.com>
2022-03-30 19:12:52 +0300

    add api/create

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Пути
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var createMessageRouter = require('./routes/api/create');
+
 
 var app = express();
 
-// view engine setup
-// app.engine('ejs', require('ejs-locals'))
-// app.use(express.static(__dirname, '/stylesheets/css'));
+
+// app.get('/routes/api/create', (req, res) => {
+//   res.json({
+//     message: 'Hello world'
+//   })
+// })
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -24,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/create', createMessageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
